32e501db372d25a54d1cfc28ef014319b565d966
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-08-31 12:18:00 -0400

    basic edits to models

---
+++ client/src/pages/Login.js
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import Auth from '../utils/auth';
---
+++ server/models/Portfolio.js
@@ -0,0 +1,6 @@
+const { Schema, model } = require("mongoose");
+
+
+const portfolioSchema = new Schema({
+
+  });
