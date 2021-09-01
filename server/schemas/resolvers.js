bb228dc7fa5698f7a922d6ac632db033769195d2
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-01 15:36:57 -0400

    edited schema thing

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,29 +25,31 @@ const resolvers = {
 //     },
 //   },
 
-//   Mutation: {
-//     addUser: async (parent, { username, email, password }) => {
-//       const user = await User.create({ username, email, password });
-//       const token = signToken(user);
-//       return { token, user };
-//     },
-//     login: async (parent, { email, password }) => {
-//       const user = await User.findOne({ email });
+  Mutation: {
+    // Sign up things & JWT
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    // logging in things
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-//       if (!user) {
-//         throw new AuthenticationError('No user found with this email address');
-//       }
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
 
-//       const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
-//       if (!correctPw) {
-//         throw new AuthenticationError('Incorrect credentials');
-//       }
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
-//       const token = signToken(user);
+      const token = signToken(user);
 
-//       return { token, user };
-//     },
+      return { token, user };
+    },
 //     addThought: async (parent, { thoughtText }, context) => {
 //       if (context.user) {
 //         const thought = await Thought.create({
@@ -115,6 +117,6 @@ const resolvers = {
 //       throw new AuthenticationError('You need to be logged in!');
 //     },
   },
-};
+}};
 
 module.exports = resolvers;
