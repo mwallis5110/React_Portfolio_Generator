f2599dc4a86b59ab03b9c11d8cdb44b4494e26fb
Carlos Perez <ca.perez.barr@gmail.com>
2021-09-02 14:05:37 -0500

    Update: Connected 'Login.js' and 'Signup.js' with useState

--- client/src/components/Login/Signup.js
+++ client/src/components/Login/Signup.js
@@ -1,36 +1,42 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class SignUp extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign Up</h3>
+function SignUp () {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
+    return (
+        <form>
+            <h3>Sign Up</h3>
 
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
+            <div className="form-group">
+                <label>First name</label>
+                <input type="text" className="form-control" placeholder="First name" onChange={(e) => {setFirstName(e.target.value)}}/>
+            </div>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+            <div className="form-group">
+                <label>Last name</label>
+                <input type="text" className="form-control" placeholder="Last name" onChange={(e) => {setLastName(e.target.value)}}/>
+            </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+            <div className="form-group">
+                <label>Email address</label>
+                <input type="email" className="form-control" placeholder="Enter email" onChange={(e) => {setEmail(e.target.value)}}/>
+            </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
-                </p>
-            </form>
-        );
-    }
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" onChange={(e) => {setPassword(e.target.value)}}/>
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+            <p className="forgot-password text-right">
+                Already registered <a href="#">sign in?</a>
+            </p>
+        </form>
+    );
 }
+
+
+export default SignUp;
