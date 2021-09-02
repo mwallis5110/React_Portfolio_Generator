519d998799c3c622b4c8e9f63e0e1575937e8888
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-09-02 11:16:59 -0500

    typeDefs compiled

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
@@ -6,14 +6,43 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
+    Portfolio: Portfolio!
   }
-  
+
+  type Portfolio {
+    _id: ID
+    portfolioText: String
+    portfolioAuthor: String
+    createdAt: String
+  }
+
   type Query {
     users: [User]
     user(username: String!): User
+    portfolio(username: String, portfolioId: ID): Portfolio
     me: User
-  }`;
+  }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addPortfolio(portfolioText: String!): Portfolio
+    removePortfolio(portfolioId: ID!): Portfolio
+  }
+`;
+
+module.exports = typeDefs;
+// type User {
+//   _id: ID
+//   username: String
+//   email: String
+//   password: String
+// }
 
 //   type AboutMe{
 //     _id: ID
@@ -51,27 +80,8 @@ const typeDefs = gql`
 //     createdAt: String
 //   }
 
-
-//   type Auth {
-//     token: ID!
-//     user: User
-//   }
-
 //   type Query {
 //     users: [User]
 //     user(username: String!): User
 //     me: User
 //   }
-
-//   type Mutation {
-//     addUser(username: String!, email: String!, password: String!): Auth
-//     login(email: String!, password: String!): Auth
-//     addThought(thoughtText: String!): Thought
-//     addComment(thoughtId: ID!, commentText: String!): Thought
-//     removeThought(thoughtId: ID!): Thought
-//     removeComment(thoughtId: ID!, commentId: ID!): Thought
-//   }
-// `;
-
-
-module.exports = typeDefs;
