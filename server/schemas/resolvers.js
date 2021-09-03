6204082874e6a44bd93236420a7980f0028cece2
EvanEbert <evan.ebert17@gmail.com>
2021-09-03 11:19:55 -0500

    added long resolver and typeDef to corresponding folders

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -51,8 +51,45 @@ const resolvers = {
     addPortfolio: async (parent, args, context) => {
       if (context.user) {
         const portfolio = await Portfolio.create({
-          args,
-          thoughtAuthor: context.user.username,
+          AboutMe: {
+            firstName: args.firstName,
+            lastName: args.lastName,
+            introduction: args.introduction
+          },
+          Education: {
+            institution: args.institution,
+            degree: args.degree,
+            graduationDate: args.graduationDate,
+          },
+          Skills: {
+            languages: args.languages,
+            programmingLanguages: args.programmingLanguages,
+            hardSkills: args.hardSkills,
+            softSkills: args.softSkills,
+          },
+          Experience: {
+            jobTitle: args.jobTitle,
+            jobDescription: args.jobDescription,
+            startDate: args.startDate,
+            endDate: args.endDate,
+            achievements: args.achievements,
+          },
+          Projects: {
+            title: args.title,
+            description: args.description,
+            images: args.images,
+            link: args.link,
+          },
+          ContactMe: {
+            phoneNumber: args.phoneNumber,
+            email: args.email,
+            linkedIn: args.linkedIn,
+            github: args.github,
+            twitter: args.twitter,
+            facebook: args.facebook,
+            instagram: args.instagram,
+          },
+          username: context.user.username,
         });
 
         await User.findOneAndUpdate(
@@ -64,56 +101,22 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    //     addComment: async (parent, { thoughtId, commentText }, context) => {
-    //       if (context.user) {
-    //         return Thought.findOneAndUpdate(
-    //           { _id: thoughtId },
-    //           {
-    //             $addToSet: {
-    //               comments: { commentText, commentAuthor: context.user.username },
-    //             },
-    //           },
-    //           {
-    //             new: true,
-    //             runValidators: true,
-    //           }
-    //         );
-    //       }
-    //       throw new AuthenticationError('You need to be logged in!');
-    //     },
-        removePorfolio: async (parent, { PortfolioId }, context) => {
-          if (context.user) {
-            const portfolio = await Portfolio.findOneAndDelete({
-              _id: PortfolioId,
-              thoughtAuthor: context.user.username,
-            });
+    removePorfolio: async (parent, {  }, context) => {
+      if (context.user) {
+        const portfolio = await Portfolio.findOneAndDelete({
+          _id: PortfolioId,
+          username: context.user.username,
+        });
 
-            await User.findOneAndUpdate(
-              { _id: context.user._id },
-              { $pull: { portfolio: Portfolio._id } }
-            );
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { portfolio: portfolio._id } }
+        );
 
-            return portfolio;
-          }
-          throw new AuthenticationError('You need to be logged in!');
-        },
-    //     removeComment: async (parent, { thoughtId, commentId }, context) => {
-    //       if (context.user) {
-    //         return Thought.findOneAndUpdate(
-    //           { _id: thoughtId },
-    //           {
-    //             $pull: {
-    //               comments: {
-    //                 _id: commentId,
-    //                 commentAuthor: context.user.username,
-    //               },
-    //             },
-    //           },
-    //           { new: true }
-    //         );
-    //       }
-    //       throw new AuthenticationError('You need to be logged in!');
-    //     },
+        return portfolio;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
   },
 };
 
