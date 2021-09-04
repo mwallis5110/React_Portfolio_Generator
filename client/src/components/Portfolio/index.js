4b6a06d03539243d40696f6cf686ca5c693d24af
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-04 16:21:27 -0400

    particle stuff

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -4,22 +4,43 @@ import { } from "react-bootstrap";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Particles from 'react-particles-js';
 
 export default function Portfolio() {
   return(
-<div className = "masterDiv">
-    <Container>
-    <Row>
-        <Col sm={4}>sm=4</Col>
-        <Col sm={8}>sm=8</Col>
-    </Row>
-    </Container>
-    <div className = "aboutMeContainer">
-        <div className = "pageOne">
-            <h2>Hello my name is </h2>
-            <h1 className = "fullName"> Joris Pow </h1>
+
+    <div className = "masterDiv">
+        <div className="particlesDiv">
+        <Particles
+                params={{
+                particles: {
+                    number: {
+                    value: 70,
+                    density: {
+                        enable: true,
+                        value_area: 1000,
+                    }
+                    },
+                },
+                }}
+            />
         </div>
+        <Container>
+            <div className = "aboutMeContainer">
+                <Row>
+                    <Col sm={4}>
+                        <div className = "pageOne">
+                            <h2>Hello my name is </h2>
+                            <h1 className = "fullName"> Joris Pow </h1>
+                        </div>
+                    </Col>
+                    <Col sm={8}>
+                        <h4> Here we can write their introduction stuff yadda yadda</h4>
+                    </Col>
+                </Row>
+            </div>
+        </Container>
     </div>
-</div>
   )
 }
+
